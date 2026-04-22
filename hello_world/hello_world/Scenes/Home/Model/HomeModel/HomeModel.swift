40e08c3a0eefb1d8e7d28c0b2046242e61d6490d
Hannah Paola Valencia Vargas <hpvalencia2003@gmail.com>
2026-04-22 17:43:37 -0600

    Correcion model

--- hello_world/hello_world/Scenes/Home/Model/HomeModel/HomeModel.swift
+++ hello_world/hello_world/Scenes/Home/Model/HomeModel/HomeModel.swift
@@ -10,6 +10,8 @@ import Foundation
 struct HomeWeather {
     let current: CurrentWeatherModel
     let hourlyForecast: [HourlyForecastModel]
-    let metrics: [WeatherMetr]
+    let metrics: [WeatherDetailsModel]
     
 }
+
+
