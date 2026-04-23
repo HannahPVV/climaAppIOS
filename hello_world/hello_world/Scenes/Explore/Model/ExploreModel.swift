bf1ee92db815cda2f34bf7c5b01ab2d43bf5c4d8
Regvalenzuela <rvalenzuela@anahuac.mx>
2026-04-22 21:27:05 -0600

    Explore
    Explore completo

--- hello_world/hello_world/Scenes/Explore/Model/ExploreModel.swift
+++ hello_world/hello_world/Scenes/Explore/Model/ExploreModel.swift
@@ -1,8 +1,53 @@
 //
-//  ..swift.swift
+//  ExploreModel.swift
 //  hello_world
 //
-//  Created by Alumnos on 20/04/26.
+//  Created by Hannah Valencia on 22/04/26.
 //
 
-import Foundation
+import UIKit
+
+// MARK: - WeatherType
+enum WeatherType: String {
+    case sunny  = "Soleado"
+    case cloudy = "Nublado"
+    case rainy  = "Lluvia"
+    case snowy  = "Nevado"
+
+    var icon: String {
+        switch self {
+        case .sunny:  return "sun.max.fill"
+        case .cloudy: return "cloud.fill"
+        case .rainy:  return "cloud.rain.fill"
+        case .snowy:  return "cloud.snow.fill"
+        }
+    }
+
+    var iconColor: UIColor {
+        switch self {
+        case .sunny:  return UIColor(red: 1.0,  green: 0.78, blue: 0.10, alpha: 1)
+        case .cloudy: return UIColor(red: 0.75, green: 0.85, blue: 0.95, alpha: 1)
+        case .rainy:  return UIColor(red: 0.65, green: 0.80, blue: 0.95, alpha: 1)
+        case .snowy:  return UIColor(red: 0.75, green: 0.88, blue: 1.00, alpha: 1)
+        }
+    }
+
+    static var allCases: [WeatherType] { [.sunny, .cloudy, .rainy, .snowy] }
+}
+
+// MARK: - LocationModel
+struct LocationModel {
+    var city: String
+    var temperature: Int
+    var weather: WeatherType
+}
+
+// MARK: - ExploreModel
+struct ExploreModel {
+    var locations: [LocationModel] = [
+        LocationModel(city: "Zibatá",      temperature: 23, weather: .cloudy),
+        LocationModel(city: "Guadalajara", temperature: 20, weather: .rainy),
+        LocationModel(city: "CDMX",        temperature: 23, weather: .sunny),
+        LocationModel(city: "Londres",     temperature:  2, weather: .snowy)
+    ]
+}
--- hello_world/hello_world/Scenes/Explore/ViewModel/ExploreViewModel.swift
+++ hello_world/hello_world/Scenes/Explore/ViewModel/ExploreViewModel.swift
@@ -2,7 +2,39 @@
 //  ExploreViewModel.swift
 //  hello_world
 //
-//  Created by Alumnos on 20/04/26.
+//  Created by Hannah Valencia on 22/04/26.
 //
 
 import Foundation
+import Combine
+
+class ExploreViewModel {
+
+    // MARK: - Model
+    private var model: ExploreModel = .init()
+
+    // MARK: - Published
+    @Published var locations: [LocationModel] = []
+
+    init() {
+        locations = model.locations
+    }
+
+    // MARK: - Public
+    func addLocation(city: String) {
+        let weather = WeatherType.allCases.randomElement() ?? .sunny
+
+        let temperature: Int = {
+            switch weather {
+            case .snowy:  return Int.random(in: -5...5)
+            case .rainy:  return Int.random(in: 8...18)
+            case .cloudy: return Int.random(in: 10...22)
+            case .sunny:  return Int.random(in: 20...35)
+            }
+        }()
+
+        let newLocation = LocationModel(city: city, temperature: temperature, weather: weather)
+        model.locations.append(newLocation)
+        locations = model.locations
+    }
+}
